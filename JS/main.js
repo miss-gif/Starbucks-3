676c04bdfa952b223e1ca4397891c6424fe2ce8e
miss-gif <ddc.akari@gmail.com>
2022-06-20 17:56:26 +0900

    header

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,43 @@
-console.log("연결확인");
-
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
+//검색 아이콘을 누르면 input이 포커스
 searchEl.addEventListener("click", function () {
   searchInputEl.focus();
 });
 
+//input에 포커스가 되면
 searchInputEl.addEventListener("focus", function () {
   searchEl.classList.add("focused");
   searchInputEl.setAttribute("placeholder", "통합검색");
 });
 
+//input이 블러가 되면
 searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
+  searchInputEl.setAttribute("value", "");
 });
+
+const badgeEl = document.querySelector(".badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to (요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
